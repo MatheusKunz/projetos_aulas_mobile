336ac91f60faae2ff3020f869f8822439e293a11
Matheus Kunz <matheus.kunz@universo.univates.br>
2025-08-20 21:47:30 -0300

    Projeto iniciado

--- Kanban/App.js
+++ Kanban/App.js
@@ -2,11 +2,7 @@ import { View, Text } from "react-native";
 
 export default function App() {
   return (
-    <View
-      style={{
-
-      }}
-    >
+    <View>
       <Text>Universal React with Expo</Text>
     </View>
   );
