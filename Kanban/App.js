8b1183670f3ec5876cd3c51352a0c3672bc947e0
Matheus Kunz <matheus.kunz@universo.univates.br>
2025-08-20 21:43:42 -0300

    Teste de git

--- Kanban/App.js
+++ Kanban/App.js
@@ -4,9 +4,7 @@ export default function App() {
   return (
     <View
       style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+
       }}
     >
       <Text>Universal React with Expo</Text>
